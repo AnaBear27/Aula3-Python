fc15edb65d77dac9235aa9cf24784be01fa8a6d9
AnaBear27 <anabeatriz6878@gmail.com>
2025-02-25 09:06:31 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+#VERIFICANDO IDADE PARA ENTRADA DE UM EVENTO.
+
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Você pode entrar no evento!")
+else:
+    print("Desculpa, você não tem idade o suficiente para entrar no evento.")
